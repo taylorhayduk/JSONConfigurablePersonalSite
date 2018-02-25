4ab98f4ef005f8aaca8fb147f5583eb765bb253e
Taylor Hayduk <taylor.hayduk@gatherhere.com>
2018-02-25 15:21:16 -0500

    Update photos and links

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var express  = require('express'),
     path = require('path'),
     app = express(),
+    port = process.env.PORT || 8034,
     dust = require('express-dustjs'),
     links = require('./links'),
     requiredInfo = require('./requiredInfo');
@@ -53,7 +54,7 @@ app.get('*', function (request, response){
     }
 });
 
-console.log('Server started on http://localhost:8080');
-app.listen(process.env.PORT || 8080);
+console.log(`Server started on http://localhost:${port}`);
+app.listen(port);
 
 module.exports = app;
